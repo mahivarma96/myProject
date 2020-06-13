4d7b1d6cfcc388a4684f6035fc77966d3b2dc5e0
mahivarma96 <66869171+mahivarma96@users.noreply.github.com>
2020-06-13 17:15:23 +0530

    Update qurantine.java
    comitting the changes to the develop branch

--- Hello World/src/com/covid/in/qurantine.java
+++ Hello World/src/com/covid/in/qurantine.java
@@ -4,6 +4,8 @@ public class qurantine {
 	public static void main(String []args){
 		boolean lockdown=true;
 		System.out.println((lockdown!=false?"Stay Home":"come out stay calm"));
+		lockdown=false;
+		System.out.println((lockdown!=false?"Haha Lockdown Extended":"come out stay calm"));
 	}
 
 }
