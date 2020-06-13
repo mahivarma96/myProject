ad143450202285bf2bc2ced2f5e78100eb8a49db
91970 <91970@DESKTOP-S6MG489>
2020-06-13 16:23:32 +0530

    initial commit message

---
+++ Hello World/src/com/covid/in/qurantine.java
@@ -0,0 +1,9 @@
+package com.covid.in;
+
+public class qurantine {
+	public static void main(String []args){
+		boolean lockdown=true;
+		System.out.println((lockdown!=false?"Stay Home":"come out stay calm"));
+	}
+
+}
